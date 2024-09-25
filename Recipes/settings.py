213c16d2cc26102dc101077d776b24b77e0844ab
e454273 <luke.kasper@lmco.com>
2024-09-25 08:16:09 -0400

    add csrf test

--- Recipes/settings.py
+++ Recipes/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'Recipes.urls'
