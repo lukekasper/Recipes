06c129b00d24cab62efb1cfcc9707b5dc69a97f0
Luke Kasper <lukekasper@Lukes-MacBook-Pro.local>
2024-12-04 16:09:14 -0500

    settings remove crop

--- Recipes/settings.py
+++ Recipes/settings.py
@@ -91,9 +91,6 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
-
 
 # Django-storages settings
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
