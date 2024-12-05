34605fce249b8e0ed8594f7b3bc76616ad664500
Luke Kasper <lukekasper@Lukes-MacBook-Pro.local>
2024-12-04 21:29:40 -0500

    media

--- Recipes/settings.py
+++ Recipes/settings.py
@@ -157,5 +157,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media settings
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
